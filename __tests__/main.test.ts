84ce40e3b1090ff2bf1bfa6bb69bc771cb0fe530
Maximilian Falkenstein <maximilian.falkenstein@beekeeper.io>
2022-06-10 15:28:34 +0200

    Fixup test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,22 +3,11 @@ import * as cp from 'child_process'
 import * as path from 'path'
 import {expect, test} from '@jest/globals'
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  //await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
-
-test('wait 500 ms', async () => {
-  const start = new Date()
-  //await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+test('can submit to stoplight', async () => {
+  process.env['INPUT_TENANT'] = 'stoplight.io/mocks/beekeeper/beekeeper-api/61115872'
+  process.env['INPUT_APIKEY'] = 'foobar'
+  process.env['INPUT_CHAT'] = '123'
+  process.env['INPUT_MESSAGE'] = 'Testmessage'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
@@ -26,3 +15,4 @@ test('test runs', () => {
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
+
